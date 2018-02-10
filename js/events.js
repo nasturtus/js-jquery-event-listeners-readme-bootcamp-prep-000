70550ca75e6b3e942aa27c85b09d8f775b7f8505
Santosh SN <nasturtus@gmail.com>
2018-02-10 03:32:48 +0000

    automatically backed up by learn

--- js/events.js
+++ js/events.js
@@ -1,7 +1,14 @@
 //define functions here
 
 $(document).ready(function(){
-
-// call functions here
-
+  
+getIt();
+  
 });
+
+  function getIt() {
+    $('p').on('click', function(){
+      alert("Hey!");
+    });
+  }
+
