d605420064e2f9e7491dff7645d0626821109eaf
Santosh SN <nasturtus@gmail.com>
2018-02-10 03:44:27 +0000

    Done.

--- js/events.js
+++ js/events.js
@@ -1,7 +1,36 @@
 //define functions here
 
 $(document).ready(function(){
-
-// call functions here
-
+  
+getIt();
+frameIt();  
+pressIt();
+submitIt();
 });
+
+function getIt() {
+  $('p').on('click', function(){
+  alert("Hey!");
+  });
+}
+
+function frameIt() {
+  $('img').on('load', function() {
+    $(this).addClass('tasty')
+  })
+}
+
+function pressIt() {
+  $(document).on('keydown', function(key){
+    if(key.which === 71) {
+      alert('You pressed G')
+    }
+  })
+}
+
+function submitIt() {
+  $('form').on('submit', function(){
+    alert("Your form is going to be submitted now.")
+  })
+}
+
